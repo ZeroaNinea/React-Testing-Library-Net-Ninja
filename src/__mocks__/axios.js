962741c26aa54ac5bb419fa96788a475718251b6
Catgirl <zeroaninea@gmail.com>
2026-06-24 15:25:13 +0400

    cries

--- src/__mocks__/axios.js
+++ src/__mocks__/axios.js
@@ -1,23 +1,30 @@
 const mockResponse = {
-    data: {
-        results: [
-            {
-                name: {
-                    first: "Laith",
-                    last: "Harb"
-                },
-                picture: {
-                    large: "https://randomuser.me/api/portraits/men/59.jpg"
-                },
-                login: {
-                    username: "ThePhonyGOAT"
-                }
-            }
-        ]
-    }
-}
+  data: {
+    results: [
+      {
+        name: {
+          first: 'Laith',
+          last: 'Harb',
+        },
+        picture: {
+          large: 'https://randomuser.me/api/portraits/men/59.jpg',
+        },
+        login: {
+          username: 'ThePhonyGOAT',
+        },
+      },
+    ],
+  },
+};
 
+// export default {
+//     get: jest.fn().mockResolvedValue(mockResponse)
+// }
 
-export default {
-    get: jest.fn().mockResolvedValue(mockResponse)
-}
+// assign object to a variable before exporting as module default
+
+const axios = {
+  get: jest.fn().mockResolvedValue(mockResponse),
+};
+
+export default axios;
--- src/components/FollowersList/__test__/FollowersList.test.js
+++ src/components/FollowersList/__test__/FollowersList.test.js
@@ -1,48 +1,63 @@
 import { render, screen, fireEvent } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
-import FollowersList from "../FollowersList";
+import FollowersList from '../FollowersList';
+
+import axios from 'axios';
 
 const MockFollowersList = () => {
-    return (
-        <BrowserRouter>
-            <FollowersList />
-        </BrowserRouter>
-    )
-}
+  return (
+    <BrowserRouter>
+      <FollowersList />
+    </BrowserRouter>
+  );
+};
 
-describe("FollowersList", () => {
-
-    beforeEach(() => {
-        // console.log("RUNS BEFORE EACH TEST")
-        jest.mock("../../../__mocks__/axios")
-    })
-
-    // beforeAll(() => {
-    //     console.log("RUNS ONCE BEFORE ALL TESTS")
-    // })
-
-    // afterEach(() => {
-    //     console.log("RUNS AFTER EACH TEST")
-    // })
-
-    // afterAll(() => {
-    //     console.log("RUNS ONCE AFTER ALL TESTS")
-    // })
-
-    it('should fetch and render input element', async () => {
-        render(
-            <MockFollowersList />
-        );
-        const followerDivElement = await screen.findByTestId(`follower-item-0`)
-        expect(followerDivElement).toBeInTheDocument();
+describe('FollowersList', () => {
+  beforeEach(() => {
+    console.log('RUNS BEFORE EACH TEST');
+    axios.get.mockResolvedValue({
+      data: {
+        results: [
+          {
+            name: {
+              first: 'Laith',
+              last: 'Harb',
+            },
+            picture: {
+              large: 'https://randomuser.me/api/portraits/men/59.jpg',
+            },
+            login: {
+              username: 'ThePhonyGOAT',
+            },
+          },
+        ],
+      },
     });
-    
-    it('should fetch and render input element', async () => {
-        render(
-            <MockFollowersList />
-        );
-    
-        const followerDivElement = await screen.findByTestId(`follower-item-0`)
-        expect(followerDivElement).toBeInTheDocument();
-    });
-})
+  });
+
+  //   beforeAll(() => {
+  //     console.log('RUNS ONCE BEFORE ALL TESTS');
+  //     jest.mock('../../../__mocks__/axios');
+  //   });
+
+  // afterEach(() => {
+  //     console.log("RUNS AFTER EACH TEST")
+  // })
+
+  // afterAll(() => {
+  //     console.log("RUNS ONCE AFTER ALL TESTS")
+  // })
+
+  it('should fetch and render input element', async () => {
+    render(<MockFollowersList />);
+    const followerDivElement = await screen.findByTestId(`follower-item-0`);
+    expect(followerDivElement).toBeInTheDocument();
+  });
+
+  it('should fetch and render input element on click', async () => {
+    render(<MockFollowersList />);
+
+    const followerDivElement = await screen.findByTestId(`follower-item-0`);
+    expect(followerDivElement).toBeInTheDocument();
+  });
+});
